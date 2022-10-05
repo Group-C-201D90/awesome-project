9fcb634e471f5e6f26eb985fb78b2ae9ef4e9e9d
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-10-05 16:34:26 +0000

    formed structure/grabbed table element

--- js/tableHomePage.js
+++ js/tableHomePage.js
@@ -1,26 +1,15 @@
-// function storyRender(storyindex){
-//   const wyos = document.getElementById('wyos');
-//   wyos.remove();
-//   let storyContent = document.createElement('p');
-//   storyContent.textContent = genretypes[storyindex][0];
-//   storyDisplay.appendChild(storyContent);
+// function tableRender(storyindex){
+//   let table = document.getElementById('bookshelf');
+//   // wyos.remove();
+//   let trElem = document.createElement('tr');
 
+//   for (let i = 0; i < genretypes.length; i++) {
+//     let thElem = document.createElement('th');
+//     let tdElem = document.createElement('td');
+
+//     // thElem.textContent = genretypes[storyindex][i][0];
+//     tdElem.textContent = genretypes[storyindex][i];
+//     trElem.appendChild(tdElem);
+//     table.appendChild(trElem);
+//   }
 // }
-
-
-function storyRender(storyindex){
-  let table = document.getElementById('bookshelf');
-  // wyos.remove();
-  let trElem = document.createElement('tr');
-
-  for (let i = 0; i < genretypes.length; i++) {
-    let tdElem = document.createElement('td');
-
-    let storyContent = document.createElement('p');
-    storyContent.textContent = genretypes[storyindex][i];
-    tdElem.appendChild(storyContent);
-    trElem.appendChild(tdElem);
-    table.appendChild(trElem);
-  }
-
-}
