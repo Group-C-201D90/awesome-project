069b6bd7761c4ed8729a7e74a5732d1a19121504
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-10-05 01:10:32 +0000

    added imageArr, removed commented codes

--- js/app-nerissa.js
+++ js/app-nerissa.js
@@ -1,8 +1,8 @@
+// image array
+let imageArr = [];
+
 // >>> DOM Manipulation <<<
 let imgArticle = document.getElementById('genre'); // images container
-// let imgFantasy = document.getElementById('fantasy');
-// let imgSciFi = document.getElementById('sci-fi');
-// let imgHorror = document.getElementById('horror');
 
 // Constructor for image selection
 function ImageGenre(name, fileExtension = 'jpg') {
