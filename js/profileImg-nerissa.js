70faca35ba2ad498d4c5335c4b84017860290459
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-10-05 00:47:31 +0000

    edit function handleImgClicked

--- js/profileImg-nerissa.js
+++ js/profileImg-nerissa.js
@@ -1,11 +1,11 @@
-let imageArr = []
+let imageArr = [];
 
 
 // >>> DOM Manipulation <<<
 let imgArticle = document.getElementById('genre'); // images container
-let imgFantasy = document.getElementById('fantasy');
-let imgSciFi = document.getElementById('sci-fi');
-let imgHorror = document.getElementById('horror');
+// let imgFantasy = document.getElementById('fantasy');
+// let imgSciFi = document.getElementById('sci-fi');
+// let imgHorror = document.getElementById('horror');
 
 // Constructor for image selection
 function ImageGenre(name, fileExtension = 'jpg') {
@@ -19,9 +19,15 @@ function handleImageClicked(event) {
   console.dir(event.target);
   let clickedImg = event.target.alt;
 
-  for (let i = 0; i < imageArr.length; i++) {
-    if (imageArr[i].name === clickedImg) {
-      
-    }
+  if (imageArr[0] === clickedImg) {
+    let genreindex = 0;  // horror
+  } else if(imageArr[1] === clickedImg) {
+    let genreindex = 1;  // fantasy
+  } else if (imageArr[2] === clickedImg) {
+    let genreindex = 2;  // sci-fi
   }
+
 }
+
+
+imgArticle.addEventListener('click', handleImageClicked);
