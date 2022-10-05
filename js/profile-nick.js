79a8074bb6b7a1dc243ffe3ece98231b6759c725
Zoe Gonzalez <zusolaris.personal@gmail.com>
2022-10-04 18:50:05 -0700

    fixed errors and got basic functionality

--- js/profile-nick.js
+++ js/profile-nick.js
@@ -1,15 +1,15 @@
-'use strict';
+// 'use strict';
 
-const storybutton = document.getElementById('storybutton');
+// const storybutton = document.getElementById('storybutton');
 
-const handleStoryButtonClick = function(){
-  const storiesFromLS = JSON.parse(localStorage.stories);
-  console.log('allCats array after retrieving from local storage', allStories);
-  for (let i = 0; i < storiesFromLS.length; i++){
-    const newStory = new Story(storiesFromLS[i].name);
-    newStory.render();
-  }
-  console.log('allStories array after reinstantiating through our Story constructor', allStories);
-};
+// const handleStoryButtonClick = function(){
+//   const storiesFromLS = JSON.parse(localStorage.stories);
+//   console.log('allCats array after retrieving from local storage', allStories);
+//   for (let i = 0; i < storiesFromLS.length; i++){
+//     const newStory = new Story(storiesFromLS[i].name);
+//     newStory.render();
+//   }
+//   console.log('allStories array after reinstantiating through our Story constructor', allStories);
+// };
 
-storybutton.addEventListener('click', handleStoryButtonClick);
+// storybutton.addEventListener('click', handleStoryButtonClick);
